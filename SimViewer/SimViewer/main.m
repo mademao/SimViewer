7842dd83509c3619eea726eddd3708341408c3f4
PlutoMa <ismademao@gmail.com>
2018-06-28 19:34:10 +0800

    修改Storyboard启动为Application的delegate启动

--- SimViewer/SimViewer/main.m
+++ SimViewer/SimViewer/main.m
@@ -7,7 +7,14 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
 int main(int argc, const char * argv[]) {
+    //本项目旨在创建一个状态条常驻功能，不需要任何window，所以无需设置启动storyboard（即设置Main Interface为空）。
+    //为了保证在没有启动storyboard的同时能正常运行项目，需要在此设置代理来保证启动
+    NSApplication *application = [NSApplication sharedApplication];
+    AppDelegate *appDelegate = [[AppDelegate alloc] init];
+    application.delegate = appDelegate;
+    
     return NSApplicationMain(argc, argv);
 }
