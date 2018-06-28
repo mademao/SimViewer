3c3356e60eaf9faa5f7360f7ecb9cc4c539eca40
PlutoMa <ismademao@gmail.com>
2018-06-28 19:56:43 +0800

    Add initialize NSStatusItem code

--- SimViewer/SimViewer/AppDelegate.m
+++ SimViewer/SimViewer/AppDelegate.m
@@ -10,12 +10,23 @@
 
 @interface AppDelegate ()
 
+//状态栏item，此变量需要强引用，否则在释放之后同时从状态栏消失
+@property (nonatomic, strong) NSStatusItem *statusItem;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    //创建状态栏item
+    //长度使用NSVariableStatusItemLength/NSSquareStatusItemLength
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    self.statusItem.image = [NSImage imageNamed:@"StatusItem"];
+    self.statusItem.image.template = YES;
+    //是否支持高亮模式
+    self.statusItem.highlightMode = YES;
+    //是否可交互
+    self.statusItem.enabled = YES;
 }
 
 
