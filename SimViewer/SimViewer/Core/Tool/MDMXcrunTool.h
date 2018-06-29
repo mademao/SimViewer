19907d9989fe5400e5ae07af53f0549bb7f3c766
PlutoMa <ismademao@gmail.com>
2018-06-29 16:35:58 +0800

    Add MDMXcrunTool

---
+++ SimViewer/SimViewer/Core/Tool/MDMXcrunTool.h
@@ -0,0 +1,15 @@
+//
+//  MDMXcrunTool.h
+//  SimViewer
+//
+//  Created by 马德茂 on 2018/6/29.
+//  Copyright © 2018年 sogou. All rights reserved.
+//  xcrun 命令相关
+
+#import <Foundation/Foundation.h>
+
+///xcrun命令所在位置
+extern NSString * const MDMXcrunCommandPath;
+
+///simctl参数
+extern NSString * const MDMXcrunSimctlArgument;
---
+++ SimViewer/SimViewer/Core/Tool/MDMXcrunTool.m
@@ -0,0 +1,15 @@
+//
+//  MDMXcrunTool.m
+//  SimViewer
+//
+//  Created by 马德茂 on 2018/6/29.
+//  Copyright © 2018年 sogou. All rights reserved.
+//
+
+#import "MDMXcrunTool.h"
+
+///xcrun命令所在位置
+NSString * const MDMXcrunCommandPath = @"/usr/bin/xcrun";
+
+///simctl参数
+NSString * const MDMXcrunSimctlArgument = @"simctl";
